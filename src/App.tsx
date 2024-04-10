a7b86d86b693edc02f75be3252cb5d71ae950994
Artyom <art161994@yandex.ru>
2024-04-10 21:12:03 +0200

    Start of project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//import React from 'react';
+import './App.css';
+import { AddForm } from './copmonents/add-form/add-form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AddForm/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/copmonents/add-form/add-form.tsx
@@ -0,0 +1,19 @@
+//import * as React from 'react'
+
+export const AddForm = () => {
+    return (
+        <div className='app-form'>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur sit amet hendrerit dolor, non condimentum nunc.
+                Sed facilisis tellus sem, eget laoreet dui consequat bibendum.
+                Praesent commodo erat mi, nec tempor enim tincidunt eget.
+                Quisque iaculis ornare enim eget finibus. Proin posuere lorem elit,
+                in venenatis nibh scelerisque sed. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Quisque malesuada, dui commodo suscipit bibendum,
+                tortor libero commodo odio, molestie fringilla magna lacus et massa.
+                Pellentesque nisi ligula, ultrices ac tincidunt sit amet, facilisis non dolor.
+                Proin finibus id est in ullamcorper. Donec consequat semper tincidunt.
+            </p>
+        </div>
+    )
+}
