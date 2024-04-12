37a63c32134a878466eed288f3be3b45a410241d
Artyom <art161994@yandex.ru>
2024-04-12 22:04:47 +0200

    adding files, html taggsand styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-//import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
 import { AddForm } from './copmonents/add-form/add-form';
 
-function App() {
+const App: FC = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <AddForm/>
-      </header>
+        <div className='header'>
+          <h1>ToDo</h1>
+        </div>
+        <div>
+          <div className='addForm'>
+            <AddForm />
+          </div>
+          <div className='worksList'></div> 
+        </div>
+        
     </div>
   );
 }
--- src/copmonents/add-form/add-form.tsx
+++ src/copmonents/add-form/add-form.tsx
@@ -2,18 +2,11 @@
 
 export const AddForm = () => {
     return (
-        <div className='app-form'>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur sit amet hendrerit dolor, non condimentum nunc.
-                Sed facilisis tellus sem, eget laoreet dui consequat bibendum.
-                Praesent commodo erat mi, nec tempor enim tincidunt eget.
-                Quisque iaculis ornare enim eget finibus. Proin posuere lorem elit,
-                in venenatis nibh scelerisque sed. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Quisque malesuada, dui commodo suscipit bibendum,
-                tortor libero commodo odio, molestie fringilla magna lacus et massa.
-                Pellentesque nisi ligula, ultrices ac tincidunt sit amet, facilisis non dolor.
-                Proin finibus id est in ullamcorper. Donec consequat semper tincidunt.
-            </p>
+        <div className='addForm'>
+            <div className="inputContainer">
+                <input type="text" placeholder="add smth"></input>
+                <button>add</button>
+            </div>
         </div>
     )
 }
